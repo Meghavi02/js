c1098d60556b29916f71393320733479060428da
Meghavi02 <jaykeraliya0@gmail.com>
2024-05-15 13:33:59 +0530

    conversion of datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = null
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//"33" => 33
+//"33abc => Nan(not a number)"
+//tur => 1; false => 0
+
+
+let isLoggedIn = "meghavi"
+let booleanisLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanisLoggedIn);
+
+//1 => true; 0 => false
+//"" => false
+//"meghavi" => true
+
+let someNumber = 77
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
