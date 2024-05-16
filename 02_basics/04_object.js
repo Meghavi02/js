3e75726aaeda778224776e547ac4817723054b6e
Meghavi02 <jaykeraliya0@gmail.com>
2024-05-16 14:08:05 +0530

    json

--- 02_basics/04_object.js
+++ 02_basics/04_object.js
@@ -43,9 +43,25 @@ const user = [
 ]
 
 user[1].email;
-console.log(tinderUser);
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+// console.log(tinderUser);
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty('isLogged'));
+// console.log(tinderUser.hasOwnProperty('isLogged'));
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "meghavi"
+}
+
+// course.courseInstructor
+const {courseInstructor: instructor} = course;
+console.log(instructor);
+
+// {
+//     "name": "meghhavi",
+//     "coursename": " js in hindi",
+//     "price": "free"
+// }
