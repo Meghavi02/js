efa56fc3d80e867a215d5914ae97900960fb9c65
Meghavi02 <jaykeraliya0@gmail.com>
2024-05-19 09:07:09 +0530

    theory(js)

---
+++ 08_events/eventbasics.js
@@ -0,0 +1,21 @@
+//javascript 
+//  -synchronuos
+//  -single threaded
+//  -default 
+
+//execution context 
+//  -execute one line of code at a time 
+//  -console log -> 1
+//  -console log -> 2
+//  -each operation waits for the last one to complete     before executing 
+//  call stack 
+//  memory help
+
+
+//Blocking code (synchronuos)
+//    =>block the flow of the program
+//    =>read the file sync
+
+//Non-blocking code (asyncronuos)
+//  =>does not block execution 
+//  =>read the file async
